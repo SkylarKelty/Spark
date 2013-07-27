ad314d83af3aa94e67b784d74bd1a6a46e65b19f
Skylar Kelty <keiouu@gmail.com>
2013-07-27 10:21:57 +0100

    Add base src

---
+++ src/Spark/Core/Spark.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Spark is a high-performance templating engine designed to work
+ * with common WYSIWYG editors.
+ *
+ * @author Skylar Kelty <skylarkelty@gmail.com>
+ */
+
+namespace Spark\Core;
+
+/**
+ * The core of Spark
+ */
+class Spark
+{
+	/**
+	 * Initialise Spark
+	 */
+	public function __construct() {
+		// 
+	}
+}
