13ff8c0645abfd15bcdb5cc888cbea4601e119e5
Skylar Kelty <skylarkelty@gmail.com>
2013-08-03 10:55:51 +0100

    Bump version for dev

--- src/Spark/Core/Spark.php
+++ src/Spark/Core/Spark.php
@@ -41,7 +41,7 @@ class Spark
 
 		// Add a demo tag
 		$this->addTag("Version", function($html, $inner) {
-			return "<p>Spark Version 0.3_alpha</p>";
+			return "<p>Spark Version 1.0_dev</p>";
 		});
 	}
 
