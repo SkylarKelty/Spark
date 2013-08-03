30d8c0d69a3d2210e8ae3dd4ce165c1aa669c113
Skylar Kelty <skylarkelty@gmail.com>
2013-08-03 07:26:40 +0100

    Add test for addTag

--- tests/Spark/Core/SparkTest.php
+++ tests/Spark/Core/SparkTest.php
@@ -7,7 +7,15 @@ class SparkTest extends PHPUnit_Framework_TestCase
       $this->_spark = new Spark\Core\Spark();
    }
    
-   public function testLoadedTags() {
+   public function testLoadingTags() {
       $this->assertEquals(1, count($this->_spark->getTags()));
+
+		$this->_spark->addTag("Test", function($html, $inner) { return "Test"; });
+
+      $this->assertEquals(2, count($this->_spark->getTags()));
+
+		$this->_spark->addTag("Test", function($html, $inner) { return "Test 2"; });
+
+      $this->assertEquals(2, count($this->_spark->getTags()));
    }
 }
